90d42677bc757148b1e881562bfd4c6f2e679dd5
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-06-27 00:15:37 -0300

    refactor: inverção de dependências, remover as menções do Prisma de dentro do caso de uso

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,12 +1,22 @@
 import { prisma } from '@/libs/prisma'
 import { Prisma } from '@prisma/client'
+import { UsersRepository } from '@/repositories/users-repository'
 /* Quando o Prisma gera a tipagem automática das tabelas ele cria alguns 
 tipos que determinam quais são as informações que precisamos enviar
 para cada ação. Utilizamos esses tipos para não ficarmos reescrevendo
 essa informação. 
 Por exemplo: Para criar um usuário ele cria o tipo userCreateInput */
 
-export class PrismaUsersRepository {
+export class PrismaUsersRepository implements UsersRepository {
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+    return user
+  }
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
