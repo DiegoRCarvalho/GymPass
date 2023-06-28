206ded324ea53f8ee108c4d3069481e046c241ad
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-06-28 19:21:19 -0300

    refactor: criar um handler para erros não tratados

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
 import fastify from 'fastify'
 import { appRoutes } from '@/http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+// Abaixo, o parâmetro request não foi usado, logo podemos substituí-lo por um underline.
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here he should log to an external tool like DataDog/NewRelic/Sentry
+  }
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
