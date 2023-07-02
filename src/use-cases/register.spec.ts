700da5d5fddfd150fb9a1beb4b0d4dceb867ecf6
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-07-01 21:41:26 -0300

    test: verificar se não é possível cadastrar o mesmo email duas vezes

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
 import { InMemoryUserRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 describe('Register Use Case', () => {
   it('should hash user password upon registration', async () => {
@@ -18,5 +19,24 @@ describe('Register Use Case', () => {
       user.password_hash,
     )
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able register with same email twice', async () => {
+    const usersRepository = new InMemoryUserRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+    const email = 'teste01@email.com'
+
+    await registerUseCase.execute({
+      name: 'Teste01',
+      email,
+      password: '123456',
+    })
+    expect(() =>
+      registerUseCase.execute({
+        name: 'Teste01',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
